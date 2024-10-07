085e0b098d2b2e0ad6ee97c2de156e329632ddda
Zua <gimzua0228@gmail.com>
2024-10-07 21:42:13 +0900

    환경 셋팅

---
+++ src/test/java/com/netcoretech/netfaulttracker/NetFaultTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcoretech.netfaulttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetFaultTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
